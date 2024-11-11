1c1d3bbeab115841ac1e7b4afa8a45ae3ef54afe
alexzandre974 <alexzandrelh@gmail.com>
2024-11-11 15:14:52 +0100

    Added a fourth option, 'Custom' allowing users to specify: Maximum number / Hot / Warm number range

--- Random_Number_Hot-Cold_en.py
+++ Random_Number_Hot-Cold_en.py
@@ -6,38 +6,47 @@ Information:
 - Bingo / Hot / warm / Cold
 """
 
-Maximum_number = 0
+Maximum_number = 1
 
 print("""The goal of the game is to guess the correct number using the "Hot/Cold" information :
       There are 3 levels :
       1) Easy : Numbers 1 to 100. Hot = within 10, Warm = within 20 and Cold for the rest.
       2) Medium : Numbers 1 to 200  and the distance is Hot=20, Warm=40, Cold
       3) Hard : Numbers 1 to  300 and the distance is Hot=30, Warm=60, Cold
+      4) Custom  : You can choose the maximum number and the distance for Hot and Warm levels
+
       """)
 
-Difficulty = int(input("Choose a difficulty (1, 2, or 3): "))
-while  Difficulty not in [1, 2, 3]:
+Difficulty = int(input("Choose a difficulty (1, 2, 3 or 4): "))
+while  Difficulty not in [1, 2, 3, 4]:
     print("Invalid choice, please choose a number between 1 and 3")
-    Difficulty = int(input("Choose a difficulty (1, 2, or 3): "))
+    Difficulty = int(input("Choose a difficulty (1, 2, 3 or 4): "))
     
-    if Difficulty  == 1:
-        print("You choose the easy difficulty")
-        Hot, Warm, Maximum_number = 10, 20, 100
+if Difficulty  == 1:
+    print("You choose the easy difficulty")
+    Hot, Warm, Maximum_number = 10, 20, 100
     
-    elif Difficulty  == 2:
-        print("You choose the medium difficulty")
-        Hot, Warm, Maximum_number = 20, 40, 200
+elif Difficulty  == 2:
+    print("You choose the medium difficulty")
+    Hot, Warm, Maximum_number = 20, 40, 200
 
-    else:
-        print("You choose the hard difficulty")
-        Hot, Warm, Maximum_number = 30, 60, 300
+elif Difficulty  == 3:
+    print("You choose the hard difficulty")
+    Hot, Warm, Maximum_number = 30, 60, 300
+    
+else :
+    print("You chose the custom difficulty")
+    Maximum_number = int(input("Enter the maximum number: "))
+    Hot = int(input("Enter the distance for Hot level: "))
+    Warm = int(input("Enter the distance for Warm level: "))
 
-
+print (f"Maximum number: {Maximum_number}, Hot distance: {Hot}, Warm distance: {Warm}")
 
 
 def Guess_a_number():
     Guess_a_number = random.randint(1, Maximum_number)
     print("A number has been chosen. Try to guess it!")
+    
     #To help me test my program
     print("Number to guess: ", Guess_a_number)
 
@@ -45,7 +54,7 @@ def Guess_a_number():
 
     User_Number = int(input("Guess a number: ")) 
     
-    while Guess_a_number != User_Number and 1 <= User_Number <= 100:
+    while Guess_a_number != User_Number and 1 <= User_Number <= Maximum_number:
         if Guess_a_number > User_Number:
             difference = Guess_a_number - User_Number
             if difference <= Hot:
